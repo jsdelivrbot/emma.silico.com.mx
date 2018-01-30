1f419e370ba7b14a2f0b969ca4874e6c5a3985bd
Marco Antonio Santana <marco.santana@gmail.com>
2018-01-30 10:09:42 -0600

    Removed comments

--- resources/views/exams/videos_partial.blade.php
+++ resources/views/exams/videos_partial.blade.php
@@ -11,10 +11,3 @@
         <source src="{{ asset('videos/'.$video->source) }}" type="video/{{pathinfo($video->source, PATHINFO_EXTENSION)}}">;
        Este navegador no es compatible con la reproducción de videos
    </video> 
-{{--  <video width="100%"  controls autoplay loop>
-    <source src="/videos/{{ $source }}.mp4" type="video/mp4">
-    <source src="/videos/{{$source}}.webm" type="video/webm">
-    <source src="/videos/{{$source}}.ogg" type="video/ogg">
-
-    El video no puede ser reproducido en este equipo por favor solicite apoyo a nuestro personal
-</video>  --}}
