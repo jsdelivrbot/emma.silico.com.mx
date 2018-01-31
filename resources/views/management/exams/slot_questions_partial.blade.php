2b3d3429fc4d6e9c2f009dc1567a169b17c745f7
Marco Antonio Santana <marco.santana@gmail.com>
2018-01-31 08:36:57 -0600

    Added video capabilities to main view

--- resources/views/management/exams/slot_questions_partial.blade.php
+++ resources/views/management/exams/slot_questions_partial.blade.php
@@ -1,11 +1,13 @@
 <div class="">
     <span class="badge">{{ $question->id }}</span>
-    <span class="badge">{!! $question->order !!}</span>{!! $question->text !!}
+    <span class="badge">{!! $question->order !!}</span><span>{!! $question->text !!}</span>
     <div class="">
             @foreach($question->images as $image)
                     <img src="{{ asset('images/exams/'.$question->slot->exam_id.'/'.$image->source) }}" class="img-thumbnail" width="65px" alt=""> 
             @endforeach
-
+            @foreach($question->videos as $video)
+                <source src="{{ asset('videos/'.$video->source) }}" type="video/{{pathinfo($video->source, PATHINFO_EXTENSION)}}">;
+            @endforeach
     </div>
     <div class="panel" style="">
         @include('management.exams.dz_question_image_form')
