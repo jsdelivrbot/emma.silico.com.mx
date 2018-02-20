5f1557b27c56b83b8dbceba0c34a00195030761a
Marco Antonio Santana <marco.santana@gmail.com>
2018-02-20 12:50:15 -0600

    Improved color and scale

--- resources/views/management/exams/progress_chart_partial.blade.php
+++ resources/views/management/exams/progress_chart_partial.blade.php
@@ -1,6 +1,7 @@
 <script src="{{ asset('js/Chart.min.js')  }}"></script>
-
-<canvas id="all_progress"></canvas>
+<div class="container">
+    <canvas id="all_progress"></canvas>
+</div>
 <script>
     (function() {
          var ctx = document.getElementById('all_progress').getContext('2d');
@@ -21,30 +22,44 @@
                   fillColor: "rgba(220,220,220,0)",
                   radius: 0,
                   //backgroundColor: "rgba(0,0,0,0.1)",
-                  showTooltips: true,
+                  showTooltips: false,
               },
               {
                 data: {{ json_encode($progress) }},
-                fillColor : "rgba(220,220,220,255)",
-                strokeColor : "#A37079",
-                pointColor : "#BC808B",
+                fillColor : "rgba(255,83,13,0.5)",
+                strokeColor : "#E82C0C",
+                pointColor : "#FF0000",
                 showTooltips: true,
               }
           ]
          };
-
+         var max =300;
+         var steps = max/100;
          new Chart(ctx).Line(chart, {
            responsive: true,
+           scaleOverride: true,
+    scaleSteps: steps,
+    scaleStepWidth: Math.ceil(max / steps),
+    scaleStartValue: 0,
            options: {
-        layout: {
-            padding: {
-                left: 200,
-                right: 0,
-                top: 0,
-                bottom: 0
-            }
-        }
-    }
+            layout: {
+                padding: {
+                    left: 0,
+                    right: 0,
+                    top: 0,
+                    bottom: 0
+                },
+                margin: {
+                    left: 50,
+                    top: 20,
+                    right: 50,
+                    bottom: 20
+                }
+                }
+           },
+           scales: {
+        yAxes: [{id: 'y-axis-1', type: 'linear', position: 'left', ticks: {min: 0, max:100}}]
+      }
          });
     })();
 </script>
